fc85cbfc5021921c13b422aa25d0c97d1fe9ee85
Neyma <neymar.queiroz@escola.pr.gov.br>
2026-03-19 13:49:23 +0000

    .

--- tipo-numero.js
+++ tipo-numero.js
@@ -6,3 +6,17 @@ const segundoNumero = 2;
 const operacaoMatematica = primeiroNumero + segundoNumero;
 
 console.log(operacaoMatematica);
+
+// Ponto flutuante
+
+const numeroPontoflutuante = 3.3;
+const numeroPontoFlutuanteSemZero = .5;
+
+const novaOPeracao = primeiroNumero / numeroPontoflutuante;
+
+console.log (novaOPeracao);
+
+// nan -> Not a Number
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
